5ff49bcd0e2afff2b632853c9e524acbcc9d8259
MarharytaBoichenko <boichenkomv@gmail.com>
2021-09-23 19:21:34 +0300

    fix  css

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,10 +1,15 @@
 export  default function onImgClick(e) {
     e.preventDefault();
+     console.log(e);
+    console.log(e.target);
     console.log(e.target.dataset.source);
+    if (e.target.nodeName !== "IMG") {
+        return;
+    }
    
 const instance = basicLightbox.create(`
     <div class="modal">
-        <img class="modal_img"
+    <img class="modal_img"
       src=${e.target.dataset.source}
       alt=${e.target.alt}
     />
@@ -12,6 +17,6 @@ const instance = basicLightbox.create(`
 `);
     instance.show();
     window.addEventListener('keydown', () => {
-        instance.close()
+    instance.close()
     })
 }
